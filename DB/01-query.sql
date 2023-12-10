1e905872b52fec1b142013ced75a578c114a27af
dvchinh <npe.etc@gmail.com>
2023-12-10 18:00:40 +1100

    Dec 10, 2023: DB

---
+++ DB/01-query.sql
@@ -0,0 +1,40 @@
+--# [castle]
+	-- origin: { siege-start-date: '2023-08-26 09:37:09.597', siege-end-date: '2023-09-02 09:37:09.597' }
+	SELECT TOP 10 * FROM MuCastle_DATA WITH(NOLOCK);
+	UPDATE MuCastle_DATA SET
+		SIEGE_START_DATE = DATEADD(day, DATEDIFF(day, SIEGE_START_DATE, GETDATE()), SIEGE_START_DATE),
+		SIEGE_END_DATE = DATEADD(day, DATEDIFF(day, SIEGE_END_DATE, DATEADD(day, 7, GETDATE())), SIEGE_END_DATE)
+	;
+
+--# [character]
+	SELECT TOP 1000 C.AccountID, C.Name, C.Class, C.ResetCount, C.Clevel, C.LevelUpPoint, C.Money, C.RuudMoney, C.PkLevel, C.PKcount
+	FROM Character C WITH(NOLOCK)
+	WHERE 1 = 1
+		AND C.Name = 'LouLx3'
+	ORDER BY C.ResetCount DESC;
+
+--# [account]
+	SELECT memb___id, memb__pwd, * FROM MEMB_INFO WHERE memb___id = 'loukoala';
+	SELECT * FROM BanMacInfo WHERE HardwareId = ?
+
+--# [reset.history]
+	SELECT TOP 100 * FROM np_reset_history WITH(NOLOCK) ORDER BY id DESC;
+	;WITH cte AS (
+		SELECT H.account, H.character
+			, COUNT(1) total
+			, MIN(H.created_at) AS created_min
+			, MAX(H.created_at) AS created_max
+		FROM np_reset_history H WITH(NOLOCK)
+		WHERE CAST(H.created_at AS DATE) = '2023-12-10'
+		GROUP BY H.account, H.character)
+	SELECT H.*, C.ResetCount
+	FROM cte H
+		INNER JOIN Character C WITH(NOLOCK) ON C.AccountID = H.account AND C.Name = H.character
+	ORDER BY H.total DESC, C.ResetCount DESC;
+
+--#
+	SELECT @@VERSION AS version;
+
+--#
+	USE MuOnline;
+	SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME IN ('Character') AND COLUMN_NAME LIKE '%app%';
---
+++ DB/01-update.sql
@@ -0,0 +1,30 @@
+﻿--# [reset.increase]
+	DECLARE @account VARCHAR(10), @character VARCHAR(10), @reset INT, @point INT
+	DECLARE cursor1 CURSOR FOR
+		SELECT TOP 100 C.AccountID, C.Name, C.ResetCount, C.LevelUpPoint
+		FROM Character C WITH(NOLOCK)
+		WHERE C.Name IN ('LouLx3', '(z)Galleon', '(z)Bing', '(z)Darkrai')
+	OPEN cursor1
+	FETCH NEXT FROM cursor1 INTO @account, @character, @reset, @point
+	WHILE @@FETCH_STATUS = 0 BEGIN
+		PRINT CONCAT('-- account = ', @account, ', character = ', @character, ', resets = ', @reset, ', points = ', @point)
+		DECLARE @r_increase INT = 0
+		IF @character = 'Galleon' BEGIN
+			SET @r_increase = 4
+		END
+		IF @character IN ('LouLx3', 'Bing', 'Darkrai') BEGIN
+			SET @r_increase = 1
+		END
+		SET @reset = @reset + @r_increase
+		SET @point = @point + (@r_increase * 500)
+		PRINT CONCAT('   > [ new ] resets = ', @reset, ', points = ', @point)
+
+		UPDATE TOP(1) Character SET
+		ResetCount = @reset, LevelUpPoint = @point
+		WHERE AccountID = @account AND Name = @character
+
+		FETCH NEXT FROM cursor1 INTO @account, @character, @reset, @point
+	END
+	CLOSE cursor1
+	DEALLOCATE cursor1
+	;
