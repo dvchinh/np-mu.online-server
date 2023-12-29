ec5489c6d109373c1460fdd541ebcb1a6d6638f3
dvchinh <npe.etc@gmail.com>
2023-12-30 04:31:43 +1100

    Dec 20, 2023: Shop, add item { GL }

--- DB/01-query.sql
+++ DB/01-query.sql
@@ -1,14 +1,14 @@
 --# [character]
 	SELECT TOP 100 C.AccountID, C.Name, C.Class, C.ResetCount, C.Clevel, C.LevelUpPoint, C.Money, C.RuudMoney, C.PkLevel, C.PKcount
 		, MT3.MasterLevel, MT3.MasterPoint, MT3.MasterSkill, MT4.EnhancePoint, MT4.EnhanceSkill, MT4.EnhanceSkillPassive
+		, MS.ConnectStat
 	FROM Character C WITH(NOLOCK)
 		LEFT JOIN MasterSkillTree MT3 WITH(NOLOCK) ON MT3.Name = C.Name
 		LEFT JOIN EnhanceSkillTree MT4 WITH(NOLOCK) ON MT4.Name = C.Name
+		LEFT JOIN MEMB_STAT MS WITH(NOLOCK) ON MS.memb___id = C.AccountID
 	WHERE 1 = 1
-		AND C.Name IN ('LouLx3', 'Nephthys')
+		AND C.Name IN ('LouLx3', 'Galleon', 'Nephthys', '1hytyoudie')
 	ORDER BY C.ResetCount DESC
-	;
-	SELECT TOP 1000 * FROM MEMB_STAT
 	;
 
 --# [account]
@@ -20,7 +20,7 @@
 --# [reset.history]
 	SELECT TOP 100 H.*
 	FROM np_reset_history H WITH(NOLOCK)
-	WHERE H.character IN ('Kundunhehe')
+	WHERE H.character IN ('Galleon')
 	ORDER BY H.id DESC
 	;
 	;WITH cte AS (
@@ -29,14 +29,14 @@
 			, MIN(H.created_at) AS created_min
 			, MAX(H.created_at) AS created_max
 		FROM np_reset_history H WITH(NOLOCK)
-		WHERE CAST(H.created_at AS DATE) = '2023-12-16'
+		WHERE CAST(H.created_at AS DATE) = '2023-12-29'
 		GROUP BY H.account, H.character)
 	SELECT H.*, C.ResetCount
 	FROM cte H
 		INNER JOIN Character C WITH(NOLOCK) ON C.AccountID = H.account AND C.Name = H.character
 	ORDER BY H.total DESC, C.ResetCount DESC
 	;
-	EXEC [dbo].[WZ_GetResetInfo] @Account = '1111', @Name = 'LouLx3'
+	EXEC [dbo].[WZ_GetResetInfo] @Account = 'hoangnn1', @Name = 'Galleon'
 	;
 
 --# [castle]
